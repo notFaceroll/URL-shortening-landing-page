3ef757d487fa6ee3bc6ba8990516fcc9357e5338
notFaceroll <overhealzlol@gmail.com>
2022-04-02 21:18:27 -0300

    Applies 'after' element between Card on features section

--- src/components/FeatureCardElements.js
+++ src/components/FeatureCardElements.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 export const Card = ({ children }) => {
-  return <div className="rounded-md p-6 my-6 bg-white text-center">{children}</div>;
+  return (
+    <div className="rounded-md p-6 my-6 z-10 bg-white text-center relative flex flex-col items-center gap-4 ">
+      {children}
+    </div>
+  );
 };
 
 export const Title = ({ children }) => {
   return (
-    <h3 className="font-semibold text-neutral-veryDarkViolet text-lg">
+    <h3 className="font-semibold text-neutral-veryDarkViolet text-lg mt-8">
       {children}
     </h3>
   );
@@ -14,4 +18,12 @@ export const Title = ({ children }) => {
 
 export const Description = ({ children }) => {
   return <p className=" text-neutral-grayishViolet text-sm">{children}</p>;
+};
+
+export const Icon = ({ children }) => {
+  return (
+    <div className="absolute -top-1/4 h-24 w-24 text-primary-cyan rounded-full bg-primary-darkViolet">
+      {children}
+    </div>
+  );
 };
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Card, Title, Description } from './FeatureCardElements';
+import { Card, Title, Description, Icon } from './FeatureCardElements';
 
 export default function Features(props) {
   return (
-    <section className='p-8 text-center bg-neutral-gray flex flex-col gap-12'>
-      <h2 className=' text-neutral-veryDarkViolet font-semibold text-xl'>Advanced Statistics</h2>
-      <Description>
-        Track how your links are performing across the web with our advanced
-        statistics dashboard.
-      </Description>
+    <section className="p-8 text-center bg-neutral-100 flex flex-col gap-12 -mt-10">
+      <header className='my-4'>
+        <h2 className=" text-neutral-veryDarkViolet font-semibold text-2xl mb-4">
+          Advanced Statistics
+        </h2>
+        <Description>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </Description>
+      </header>
+      <div className='flex flex-col gap-8 items-center after:absolute after:-bottom-2/3 z-0 after:h-1/2 after:w-1 after:bg-primary-cyan'>
+
       <Card>
-        <div>icon</div>
+        <Icon>icon</Icon>
         <Title>Brand Recognition</Title>
         <Description>
           Boost your brand recognition with each click. Generic links don't mean
@@ -18,7 +24,7 @@ export default function Features(props) {
         </Description>
       </Card>
       <Card>
-        <div>icon</div>
+        <Icon>icon</Icon>
         <Title>Detailed Records</Title>
         <Description>
           Gain insighs into who is clicking your links. Knowing when and where
@@ -26,13 +32,14 @@ export default function Features(props) {
         </Description>
       </Card>
       <Card>
-        <div>icon</div>
+        <Icon>icon</Icon>
         <Title>Fully Customizable</Title>
         <Description>
           Improve brand awareness and content discoverability through
           customizable links, supercharging audience engagement.
         </Description>
       </Card>
+      </div>
     </section>
   );
 }
